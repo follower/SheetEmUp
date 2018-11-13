6faf417e8f110e851c87d885b6bc60debc9476d3
follower <follower@rancidbacon.com>
2018-11-14 09:35:02 +1300

    Don't pre-populate the property values.

--- sheet-em-up/PropertyOptionButton.gd
+++ sheet-em-up/PropertyOptionButton.gd
@@ -7,9 +7,7 @@ extends OptionButton
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	add_item("")
-	add_item("Jump X")
-	add_item("Jump Y")
+	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
