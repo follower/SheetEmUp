27494a285f4385c31284d5524dc1afaec3c8c608
follower <follower@rancidbacon.com>
2018-11-15 10:15:14 +1300

    Add note re: one-way collision elevator issue.

---
+++ sheet-em-up/CollisionShape2D.gd
@@ -0,0 +1,4 @@
+extends CollisionShape2D
+
+# Note: With Godot 3.0.6 one-way collision with upward moving platform
+#       doesn't work. A possible workaround is in <https://github.com/godotengine/godot/issues/9399>
